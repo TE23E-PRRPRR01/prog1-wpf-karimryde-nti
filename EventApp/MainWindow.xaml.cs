7129d304ca0abdf13b03c6bd473f7cab659a6366
Karim Ryde <karye.webb@gmail.com>
2025-02-05 12:52:36 +0100

    Tränat på xaml, wpf och events

---
+++ EventApp/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EventApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickOk(object sender, RoutedEventArgs e)
+    {
+        // Läs av textrutan
+        string text = tbxText.Text;
+
+        MessageBox.Show($"Du skrev: '{text}'");
+    }
+
+    private void KlickAvbryt(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("DU klickade avbryt");
+    }
+}

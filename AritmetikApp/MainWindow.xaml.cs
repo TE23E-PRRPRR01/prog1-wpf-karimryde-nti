cfdf48f48f18f3445430ffb1ff074636b264515f
Karim Ryde <karye.webb@gmail.com>
2025-03-24 14:43:11 +0100

    Tränat

---
+++ AritmetikApp/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AritmetikApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickBeräkna(object sender, RoutedEventArgs e)
+    {
+        // Läs av rutorna: txbTal1, txbTal2, txbOperator
+        string tal1String = txbTal1.Text;
+        string tal2String = txbTal2.Text;
+        string op = txbOperator.Text;
+
+        // KOnvertera till heltal
+        int.TryParse(tal1String, out int tal1);
+        int.TryParse(tal2String, out int tal2);
+
+        if (op == "+")
+        {
+            int resultat = tal1 + tal2;
+            lblResultat.Content = $"{resultat}";
+        }
+        else if (op == "-")
+        {
+            int resultat = tal1 - tal2;
+            lblResultat.Content = $"{resultat}";
+        }
+        else if (op == "*")
+        {
+            int resultat = tal1 * tal2;
+            lblResultat.Content = $"{resultat}";
+        }
+        else if (op == "/")
+        {
+            int resultat = tal1 / tal2;
+            lblResultat.Content = $"{resultat}";
+        }
+        else
+        {
+            lblResultat.Content = "Ogiltig inmatning!";
+        }
+    }
+}

6e2655d10e96917cf177fb2ab239700a955c389b
Karim Ryde <karye.webb@gmail.com>
2025-05-05 14:58:22 +0200

    Börjat med koden

---
+++ Ceasarkrypto/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ceasarkrypto;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickKryptera(object sender, RoutedEventArgs e)
+    {
+        // Läs av textrutorna txbInput & txbKey
+        string inputText = txbInput.Text;
+        string keyText = txbKey.Text;
+
+        // Konvertera keyText till key(int)
+        int.TryParse(keyText, out int key);
+
+        // Rensa txbOutput
+        txbOutput.Clear();
+
+        // Gå igenom inputText bokstav-för-bokstav
+        // Loopa bokstav-för-bokstav
+        foreach (var bokstav in inputText)
+        {
+            // Hitta ASCII-värde
+            int ascii = (int)bokstav;
+
+            // Kryptera, dvs skifta framåt
+            int asciiKrypterad = ascii + key;
+            char bokstavKrypterad = (char)asciiKrypterad;
+
+            txbOutput.Text += $"{bokstavKrypterad}";
+        }
+    }
+}
